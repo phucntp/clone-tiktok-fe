cfe5d8d1d238f90c56334877e7ebae876fc4b10d
Phucnguyen <nguyen.phuc@jvb-corp.com>
2023-02-21 00:43:42 +0700

    Login commit 3

--- src/components/atoms/form/inputs/InputNormal.tsx
+++ src/components/atoms/form/inputs/InputNormal.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 interface Props {
-  label: string;
+  label?: string;
 }
 
 function InputNormal({ label }: Props) {
   return (
     <div className="d-flex justify-center align-center">
-      <label className="font-20 w-100">{label}</label>
-      <div className="p-20 w-500-px">
-        <input />
+      {label && <label className="font-20 w-100">{label}</label>}
+      <div className="w-100">
+        <input className="w-100 normal-input"/>
       </div>
     </div>
   );
--- src/components/molecules/modal/Modal.tsx
+++ src/components/molecules/modal/Modal.tsx
@@ -3,10 +3,21 @@ import styles from "./Modal.module.scss";
 
 interface Props {
   children: React.ReactNode;
+  height?: string;
+  width?: string;
 }
 
-function Modal({ children }: Props) {
-  return <div className={styles.modalContainer}>{children}</div>;
+function Modal({ children, height, width }: Props) {
+  return (
+    <>
+      <div
+        className={styles.modalContainer}
+        style={{ height: height ?? "", width: width ?? "" }}
+      >
+       {children}
+      </div>
+    </>
+  );
 }
 
 export default Modal;
