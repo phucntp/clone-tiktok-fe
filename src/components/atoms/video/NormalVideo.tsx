0b1f5ac63606e2332fee7c91f56777ebd2f07bf5
phucntp <phucqaz.1234@gmail.com>
2023-12-02 18:07:54 +0700

    Update control video

--- src/components/atoms/video/NormalVideo.tsx
+++ src/components/atoms/video/NormalVideo.tsx
@@ -65,15 +65,15 @@ function NormalVideo({
   const onClickHandler = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    // if (event.detail === 1) {
-    //   if (state.playing) {
-    //     controls.pause();
-    //   } else {
-    //     controls.play();
-    //   }
-    //   // eslint-disable-next-line no-empty
-    // } else if (event.detail === 2) {
-    // }
+    if (event.detail === 1) {
+      if (state.playing) {
+        controls.pause();
+      } else {
+        controls.play();
+      }
+      // eslint-disable-next-line no-empty
+    } else if (event.detail === 2) {
+    }
   };
 
   const handleChangeProgress = useCallback(
